c0b117aefb2c9d18387a7a254ed1f7d87e4ca9f8
Massimo Giuliani <massgln9@gmail.com>
2022-12-02 17:09:05 +0100

    inizio setup

---
+++ database/migrations/2022_12_02_113644_create_products_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            //default tables
+            $table->string('name');
+            $table->text('description');
+            $table->float('price', 12,2);
+            $table->string('brand');
+            $table->string('slug');
+
+
+            //FOREIGN KEY
+            //user
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            //category
+           
+
+
+            $table->timestamps();
+
+
+
+
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
