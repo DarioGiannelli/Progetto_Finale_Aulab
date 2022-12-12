9a3cb52d318f941e4c143bbea0c754a76c345eee
Angelo Cristofori <angelo.cristofori.97@gmail.com>
2022-12-12 09:21:06 +0100

    vista revisione e prodotto singolo aggiornate

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -7,12 +7,12 @@
         {{ session('message') }}
     </div>
 @endif
-        <div class="col-12">
+        <div class="col-12 my-4">
             <h1> {{$product_to_check ? 'Annuncio da revisionare' : 'Nessun annuncio da revisionare.'}}</h1>
         </div>
     </div>
 </div>
-@if ($product_to_check)
+{{-- @if ($product_to_check)
 <div class="container">
     <div class="row">
         <div class="col-12">
@@ -60,6 +60,161 @@
                 </div>
             </div>
         </div>
+    </div>
+</div>
+@endif --}}
+
+
+@if ($product_to_check)
+{{-- <div class="container-fluid my-4 mt-5">
+    <div class="row">
+        <div class="col-12">
+            <h1>Prodotto selezionato</h1>
+        </div>
+    </div>
+</div> --}}
+
+<div class="container prodotto mb-5 text-dark">
+    <div class="row">
+        {{-- swiper --}}
+        <div class="col-12 col-md-6">
+            <div class="container">
+                <div class="row">
+                    {{-- swiper --}}
+                    <div class="col-12">
+                        <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff" class="swiper mySwiper2 rounded-4">
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide">
+                                <img src="https://swiperjs.com/demos/images/nature-1.jpg"/>
+                                </div>
+                                <div class="swiper-slide">
+                                <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+                                </div>
+                                <div class="swiper-slide">
+                                <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                                </div>
+                                <div class="swiper-slide">
+                                <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                                </div>
+                                <div class="swiper-slide">
+                                <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+                                </div>
+                                <div class="swiper-slide">
+                                <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+                                </div>
+                                <div class="swiper-slide">
+                                <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+                                </div>
+                                <div class="swiper-slide">
+                                <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+                                </div>
+                                <div class="swiper-slide">
+                                <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+                                </div>
+                                <div class="swiper-slide">
+                                <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
+                                </div>
+                            </div>
+                            <div class="swiper-button-next"></div>
+                            <div class="swiper-button-prev"></div>
+                            </div>
+                            <div thumbsSlider="" class="swiper mySwiper">
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide">
+                                <img src="https://swiperjs.com/demos/images/nature-1.jpg" class=" rounded-4"/>
+                                </div>
+                                <div class="swiper-slide">
+                                <img src="https://swiperjs.com/demos/images/nature-2.jpg" class=" rounded-4"/>
+                                </div>
+                                <div class="swiper-slide">
+                                <img src="https://swiperjs.com/demos/images/nature-3.jpg" class=" rounded-4"/>
+                                </div>
+                                <div class="swiper-slide">
+                                <img src="https://swiperjs.com/demos/images/nature-4.jpg" class=" rounded-4"/>
+                                </div>
+                                <div class="swiper-slide">
+                                <img src="https://swiperjs.com/demos/images/nature-5.jpg" class=" rounded-4"/>
+                                </div>
+                                <div class="swiper-slide">
+                                <img src="https://swiperjs.com/demos/images/nature-6.jpg" class=" rounded-4"/>
+                                </div>
+                                <div class="swiper-slide">
+                                <img src="https://swiperjs.com/demos/images/nature-7.jpg" class=" rounded-4"/>
+                                </div>
+                                <div class="swiper-slide">
+                                <img src="https://swiperjs.com/demos/images/nature-8.jpg" class=" rounded-4"/>
+                                </div>
+                                <div class="swiper-slide">
+                                <img src="https://swiperjs.com/demos/images/nature-9.jpg" class=" rounded-4"/>
+                                </div>
+                                <div class="swiper-slide">
+                                <img src="https://swiperjs.com/demos/images/nature-10.jpg" class=" rounded-4"/>
+                                </div>
+                            </div>
+                            </div>
+                    </div>
+                    <div class="col-12">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-md-6 d-flex flex-column">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <h2 class="h1 mt-4">{{$product_to_check->name}}</h2>
+                        <p class="fs-5 mb-4">{{$product_to_check->brand}}</p>
+
+                        <h3>Descrizione</h3>
+                        
+                        <p class="mb-4 fs-5">{{$product_to_check->description}}</p>
+
+                        <h3>Prezzo:</h3>
+
+                        <p class="mb-4 fs-5">€ {{$product_to_check->price}}</p>
+                        
+                        <h3>Caricato da:</h3>
+            
+                        <p class="mb-4 fs-5">{{$product_to_check->user->name}}</p>
+            
+                        <h3>Il:</h3>
+            
+                        <p class="mb-4 fs-5">{{$product_to_check->created_at->format('d/m/Y')}}</p>
+            
+                        <h3>Categoria:</h3>
+            
+                        <a href="{{route('categoryShow', $product_to_check->category)}}" class="btn btn-primary rounded-pill">{{$product_to_check->category->name}}</a>
+                    </div>
+                </div>
+                <div class="mt-4 row">
+                    <div class="col-12">
+                        <h4>Revisione:</h4>
+                    </div>
+                    {{-- <div class="col-12">
+                        <button class="btn btn-danger rounded-pill">Aggiungi al carrello</button>
+                    </div> --}}
+                    <div class="col-2 d-flex justify-content-center">
+                        <form action="{{Route('revisor.accept_product', ['product'=>$product_to_check])}}"method="POST">
+                            @csrf
+                            @method('PATCH')
+                           <button class="btn btn-success rounded-pill" type="submit">Accetta</button>
+                        </form>
+                    </div>
+                    <div class="col-2 d-flex justify-content-center">
+                        <form action="{{Route('revisor.reject_product', ['product'=>$product_to_check])}}"method="POST">
+                            @csrf
+                            @method('PATCH')
+                           <button class="btn btn-warning rounded-pill" type="submit">Rifiuta</button>
+                        </form>
+                    </div>
+                </div>
+                
+                
+            </div>
+        </div>
+
     </div>
 </div>
 @endif
