eefcd4fb99c78de551c4615146c5492c11a37afa
luca menconi <menconiluca2000@gmail.com>
2022-12-13 19:44:14 +0100

    pull

---
+++ lang/en/ui.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'allProducts'=>'Here\'s all our products',
+    'masthead'=>'everything you want to sell and buy...also from your sofa!',
+    'products'=>'Products',
+    'revisor'=>'Revisor zone',
+    'categories'=>'Categories',
+    'product'=>'Create product',
+    'vendi'=>'Sell now',
+    'compra'=>'Buy...SOON!',
+    'ourCategories'=>'Our categories',
+    'lastProducts'=>'Last products'
+];
---
+++ lang/it/ui.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'allProducts'=>'tutti i prodotti',
+    'masthead'=>'Tutto ciò che vuoi vendere ed acquistare... anche dal divano di casa!',
+    'products'=>'Annunci',
+    'revisor'=>'zona revisore',
+    'categories'=>'Categorie prodotti',
+    'product'=>'Crea prodotto',
+    'vendi'=>'Vendi subito',
+    'compra'=>'Compra...PRESTO!',
+    'ourCategories'=>'Le nostre categorie',
+    'lastProducts'=>'Ultimi prodotti'
+];
