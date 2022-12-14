3e6f4f4bbc7d1f0b5935b4ad701421041fd1e74e
Dario Giannelli <dario.giannelli89@gmail.com>
2022-12-14 13:08:22 +0100

    aggiornate migrazioni e seeder

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Category extends Model
 {
     use HasFactory;
-    protected $fillable = ['name', 'icon'];
+    protected $fillable = ['name', 'icon', 'name_en'];
 
     public function products()
         {
